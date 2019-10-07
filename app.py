808535c61ca38d9922a0735a00d1b653244bac35
David Chapman <chappyhova112@googlemail.com>
2019-10-07 12:16:17 +0100

    Update

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def render():
     with open('C:/Users/Chappy/PycharmProjects/FPL_Website/Data/2019-20/fpl.pickle', 'rb') as f:
         data = pickle.load(f)
-    return render_template('index.html', data=data.to_html(escape=False, index=True, classes='my_class" id = "fpl'))
+    return render_template('index.html', data=data.to_html(escape=False, index=False, classes='my_class" id = "fpl'))
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -25,9 +25,8 @@ def teams():
             data = pickle.load(f)
         new_data = form(data, request.form['last-x-games'])
         new_data = clean_df(new_data)
-        return render_template('index.html', data=new_data.to_html(escape=False, index=False, classes='my_class" id = "fpl'))
-
-
+        return render_template('index.html',
+                               data=new_data.to_html(escape=False, index=False, classes='my_class" id = "fpl'))
 
 
 @app.route('/player/<number>')
@@ -37,10 +36,7 @@ def player(number):
     return render_template('player.html', data=data.to_html(escape=False))
 
 
-
-
-
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    app.run()
 
 
@@ -86,33 +82,29 @@ def clean_players():
     for file_name in natsorted(os.listdir('Data/2019-20/players')):
         file = open('Data/2019-20/players/' + file_name, 'r')
         data = json.load(file)
-
-        data = pd.DataFrame(data['history'])
-        data = data.rename(
-            columns={'opponent_team': 'opponent', 'was_home': 'was home', 'total_points': 'total points'})
-        # Creating a column for non-appearance points
-        for index, row in data.iterrows():
-            if row['minutes'] > 59:
-                data.loc[index, 'non-appearance points'] = row['total points'] - 2
-            elif row['minutes'] > 0:
-                data.loc[index, 'non-appearance points'] = row['total points'] - 1
+        try:
+            data = pd.DataFrame(data['history'])
+            data = data.rename(
+                columns={'opponent_team': 'opponent', 'was_home': 'was home', 'total_points': 'total points'})
+            data.columns = data.columns.str.title()
+            # Creating a column for non-appearance points
+            for index, row in data.iterrows():
+                if row['Minutes'] > 59:
+                    data.loc[index, 'Non-Appearance Points'] = row['Total Points'] - 2
+                elif row['Minutes'] > 0:
+                    data.loc[index, 'Non-Appearance Points'] = row['Total Points'] - 1
+                else:
+                    data.loc[index, 'Non-Appearance Points'] = 0
+            # Saving the data required. Also preventing an error that is thrown when a new player to the Premier League has no data
+            if not data.empty:
+                data = data[['Non-Appearance Points', 'Minutes']]
+                data.to_pickle('Data/2019-20/players_pickle/{}.pickle'.format(counter))
             else:
-                data.loc[index, 'non-appearance points'] = 0
-        # Creating a column for number of times the player has played 75 minutes or more
-        for index, row in data.iterrows():
-            if row['minutes'] > 74:
-                data.loc[index, 'over 75 mins'] = True
-            else:
-                data.loc[index, 'over 75 mins'] = False
-        # Saving the data required. Also preventing an error that is thrown when a new player to the Premier League has no data
-        if not data.empty:
-            data = data[['non-appearance points', 'over 75 mins', 'minutes']]
-            save_df(data, 'Data/2019-20/players_pickle/' + str(counter) + '.pickle')
-        else:
-            data = pd.DataFrame(columns=['non-appearance points', 'over 75 mins', 'minutes'], data=[[0, 0, 0, 0, 0]])
-            print(data.columns.values)
-            save_df(data, 'Data/2019-20/players_pickle/' + str(counter) + '.pickle')
-
+                data = pd.DataFrame(columns=['Non-Appearance Points', 'Minutes'], data=[[0, 0]])
+                data.to_pickle('Data/2019-20/players_pickle/{}.pickle'.format(counter))
+        except KeyError:
+            data = pd.DataFrame(columns=['Non-Appearance Points', 'Minutes'], data=[[0, 0]])
+            data.to_pickle('Data/2019-20/players_pickle/{}.pickle'.format(counter))
         counter += 1
 
 
@@ -127,22 +119,25 @@ def json_to_df(data):
                                         'total_points': 'total points', 'first_name': 'first name',
                                         'second_name': 'second name'})
 
-    for col in ['threat', 'creativity', 'points_per_game', 'ownership']:
+    fpl_data.columns = fpl_data.columns.str.title()
+
+    for col in ['Threat', 'Creativity', 'Points_Per_Game', 'Ownership']:
         fpl_data[col] = fpl_data[col].astype(float)
 
-    fpl_data['position'] = (fpl_data['position']).astype(str)
-    fpl_data['minutes'] = (fpl_data['minutes']).astype(int)
+    fpl_data['Position'] = (fpl_data['Position']).astype(str)
+    fpl_data['Minutes'] = (fpl_data['Minutes']).astype(int)
 
     fpl_data = combining_dfs(fpl_data)
-    fpl_data = calculate_value(fpl_data)
     fpl_data = positions(fpl_data)
+    fpl_data = calculate_value(fpl_data)
     fpl_data = vapm(fpl_data)
     fpl_data = merge_names(fpl_data)
     fpl_data = form(fpl_data, 5)
-    fpl_data = calculate_value(fpl_data)
     fpl_data = formatting(fpl_data)
-    fpl_data = team_names(fpl_data, 'team')
+    fpl_data = team_names(fpl_data)
     fpl_data = clean_df(fpl_data)
+    fpl_data = rename_columns(fpl_data)
+
     fpl_data.to_pickle("Data/2019-20/fpl.pickle")
 
 
@@ -152,22 +147,31 @@ def formatting(data):
     return data
 
 
+def rename_columns(data):
+    data.columns = data.columns.str.title()
+    data = data.rename(columns={'Vapm': 'VAPM', 'Napp/90': 'NAPP/90',
+                       'Threat': 'Threat/90', 'Creativity': 'Creativity/90'})
+    return data
+
+
 def merge_names(data):
     # Merging the first and second name columns into one column
-    data['Player Name'] = data['first name'] + ' ' + data['second name']
+    data['Player Name'] = data['First Name'] + ' ' + data['Second Name']
     return data
 
 
 def vapm(data):
     # Calculating Value Added Per Million column
     data['VAPM'] = \
-        np.where(data['position'] == 'Goalkeeper', (data['non-appearance points'] / data['minutes']) / (data['price'] - 35) * 10000,
-                 np.where(data['position'] == 'Defender',
-                          (data['non-appearance points'] / data['minutes']) / (data['price'] - 35) * 10000,
-                          np.where(data['position'] == 'Midfielder',
-                                   (data['non-appearance points'] / data['minutes']) / (data['price'] - 40) * 10000,
-                                   np.where(data['position'] == 'Striker',
-                                            (data['non-appearance points'] / data['minutes']) / (data['price'] - 40) * 10000, 0))))
+        np.where(data['Position'] == 'Goalkeeper',
+                 (data['Non-Appearance Points'] / data['Minutes']) / (data['Price'] - 35) * 10000,
+                 np.where(data['Position'] == 'Defender',
+                          (data['Non-Appearance Points'] / data['Minutes']) / (data['Price'] - 35) * 10000,
+                          np.where(data['Position'] == 'Midfielder',
+                                   (data['Non-Appearance Points'] / data['Minutes']) / (data['Price'] - 40) * 10000,
+                                   np.where(data['Position'] == 'Striker',
+                                            (data['Non-Appearance Points'] / data['Minutes']) / (
+                                                    data['Price'] - 40) * 10000, 0))))
     return data
 
 
@@ -181,9 +185,9 @@ def clean_df(fpl_data):
 
 def calculate_value(data):
     # Calculating player value
-    data['value'] = (data['total points'] / data['minutes']) / data['price'] * 10000
-    data['non-appearance value'] = (data['non-appearance points'] / data['minutes']) / data['price'] * 10000
-    data['NA-PP90'] = (data['non-appearance points'] / data['minutes']) * 90
+    data['NAPP/90'] = (data['Non-Appearance Points'] / data['Minutes']) * 90
+    data['Threat/90'] = (data['Threat'] / data['Minutes']) * 90
+    data['Creativity/90'] = (data['Creativity'] / data['Minutes']) * 90
 
     return data
 
@@ -194,16 +198,10 @@ def combining_dfs(data):
     main_data = data
 
     for file_name in natsorted(os.listdir('Data/2019-20/players_pickle')):
-            data = load_df('Data/2019-20/players_pickle/' + file_name)
-            non_app_points = data['non-appearance points'].sum()
-            seventy_five_mins = data['over 75 mins'].sum()
-            big_chances_created = data['big chances created'].sum()
-            big_chances_missed = data['big chances missed'].sum()
-            main_data.loc[main_data['id'] == counter, 'non-appearance points'] = non_app_points
-            main_data.loc[main_data['id'] == counter, 'over 75 mins'] = seventy_five_mins
-            main_data.loc[main_data['id'] == counter, 'big chances created'] = big_chances_created
-            main_data.loc[main_data['id'] == counter, 'big chances missed'] = big_chances_missed
-            counter += 1
+        data = load_df('Data/2019-20/players_pickle/' + file_name)
+        non_app_points = data['Non-Appearance Points'].sum()
+        main_data.loc[main_data['Id'] == counter, 'Non-Appearance Points'] = non_app_points
+        counter += 1
 
     return main_data
 
@@ -212,49 +210,49 @@ def form(data, num_of_matches):
     # Calculating form for the number of games specified by the user
 
     # Creating the headers for each column which will be dynamic dependent on user input
-    non_appearance_header = 'Non-Appearance Points Last ' + str(num_of_matches) + ' Matches'
-    mins_header = 'Mins Last ' + str(num_of_matches) + ' Matches'
-    napp90_header = 'NA-PP90 Last ' + str(num_of_matches) + ' Matches'
+    non_appearance_header = 'Non-Appearance Points Last {} Matches'.format(num_of_matches)
+    mins_header = 'Mins Last {} Matches'.format(num_of_matches)
+    napp90_header = 'NAPP/90 Last {} Matches'.format(num_of_matches)
 
     # Iterating through each player and calculating their form which is non-appearance points, minutes played & non-apperance points per 90 minutes over the last x games
     counter = 1
     main_data = data
     for file_name in natsorted(os.listdir('Data/2019-20/players_pickle')):
         data = load_df('Data/2019-20/players_pickle/' + file_name)
-        mins_last_x = data['minutes'][-int(num_of_matches):].sum()
-        for col in data.columns:
-            print(col)
-        non_appearance_last_x = (data['non-appearance points'][-int(num_of_matches):].sum() / data['minutes'][-int(num_of_matches):].sum()) / main_data.loc[main_data['Player Name'] == counter, 'price'] * 10000
-        napp90_last_x = (data['non-appearance points'][-int(num_of_matches):].sum() / data['minutes'][-int(num_of_matches):].sum()) * 90
 
-        main_data.loc[main_data['id'] == counter, napp90_header] = napp90_last_x
-        main_data.loc[main_data['id'] == counter, non_appearance_header] = non_appearance_last_x
-        main_data.loc[main_data['id'] == counter, mins_header] = mins_last_x
+        mins_last_x = data['Minutes'][-int(num_of_matches):].sum()
+        non_appearance_last_x = data['Non-Appearance Points'][-int(num_of_matches):].sum()
+        napp90_last_x = (data['Non-Appearance Points'][-int(num_of_matches):].sum() / data['Minutes'][-int(num_of_matches):].sum()) * 90
+
+        main_data.loc[main_data['Id'] == counter, napp90_header] = napp90_last_x
+        main_data.loc[main_data['Id'] == counter, non_appearance_header] = non_appearance_last_x
+        main_data.loc[main_data['Id'] == counter, mins_header] = mins_last_x
+        print(file_name)
         counter += 1
 
     # Returning the data with the new headers
-    main_data = main_data[['Player Name', 'VAPM', 'Minutes', mins_header, 'Price', 'Total Points', 'Ownership',
-                         'position', 'threat', 'creativity', 'team', non_appearance_header, 'non-appearance points',
-                         'NA-PP90', napp90_header, 'over 75 mins', 'big chances created', 'big chances missed']]
+    main_data = main_data[['Id', 'Price', 'Player Name', 'Position', 'Team', 'VAPM', 'NAPP/90', 'Minutes',
+                           'Total Points', 'Non-Appearance Points',
+                           'Ownership', 'Threat/90', 'Creativity/90', mins_header, non_appearance_header,
+                           napp90_header]]
     return main_data
 
 
 def positions(data):
     # Changing element types to positions
     vals_to_replace = {'1': 'Goalkeeper', '2': 'Defender', '3': 'Midfielder', '4': 'Striker'}
-    data['position'] = data['position'].map(vals_to_replace)
+    data['Position'] = data['Position'].map(vals_to_replace)
 
     return data
 
 
-def team_names(data, name):
+def team_names(data):
     # Converting the integers to team names
-    data[name] = (data[name].replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+    data['Team'] = (data['Team'].replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
                                           14, 15, 16, 17, 18, 19, 20],
-                                         ['Arsenal', 'Bournemouth', 'Brighton', 'Burnley', 'Cardiff',
-                                          'Chelsea',
-                                          'C Palace', 'Everton', 'Fulham', 'Huddersfield', 'Leicester',
-                                          'Liverpool', 'Man City', 'Man Utd', 'Newcastle', 'Southampton',
+                                         ['Arsenal', 'Aston Villa', 'Bournemouth', 'Brighton', 'Burnley',
+                                          'Chelsea', 'C Palace', 'Everton', 'Leicester', 'Liverpool', 'Man City',
+                                          'Man Utd', 'Newcastle', 'Norwich', 'Sheff Utd', 'Southampton',
                                           'Spurs', 'Watford', 'West Ham', 'Wolves']))
 
     return data
@@ -268,8 +266,6 @@ def run_data():
     json_to_df('Data/2019-20/fpl.json')
 
 
-
-
 def save_df(data, filepath):
     data.to_pickle(filepath)
 
@@ -281,6 +277,3 @@ def load_df(filepath):
 
 
 run_data()
-
-
-
